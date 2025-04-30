554a1ed7e76661b551dad437497dcdb1aa761b55
Mendes8504 <mendess.8677@gmail.com>
2025-04-30 14:26:35 +0000

    Implementação nivel Aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,42 +1,51 @@
 #include <stdio.h>
+#define linha 10
+#define coluna 10
 
-int main() {
-    int tabuleiro[10][10];
+int main(){
 
-    // Inicializando a matriz com zeros
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-            tabuleiro[i][j] = 0;
-        }
+    int matriz [10][10];//Definindo o tamanho da matriz
+
+    //Definindo o valor de zero para toda a matriz
+    for (int i = 0; i < linha; i++){
+        for (int j = 0; j < coluna; j++){
+            matriz[i][j] = 0;
+    }}
+        
+    //defenindo o lugar do navio na vertical
+    for(int i = 7; i < 10; i++){
+            matriz[i][9] = 3;
     }
 
-    // Posicionando um navio horizontalmente (3 espaços)
-    int navio1x = 2, navio1y = 5;
-    for (int i = 0; i < 3; i++) {
-        tabuleiro[navio1x][navio1y + i] = 3;
+    //Defenindo o lugar do navio na horizontal
+    for (int i = 4; i < 7; i++){
+            matriz[4][i] = 3;
     }
+        
 
-    // Posicionando um navio verticalmente (3 espaços)
-    int navio2x = 6, navio2y = 3;
-    for (int i = 0; i < 3; i++) {
-        tabuleiro[navio2x + i][navio2y] = 3;
+     //exibição das letras
+    printf("   "); //espaçamento para ajustar as letras com a matriz
+    for (char letra = 'A'; letra <= 'J'; letra++){
+        printf(" %c", letra);
     }
+        
+        printf("\n");//quebra de linha para deixar matriz abaixo das letras
 
-    // Exibir letras no topo
-    printf("    "); // Espaço para alinhar com a numeração vertical
-    for (char letra = 'A'; letra <= 'J'; letra++) {
-        printf("%2c ", letra);
+    //Exibição da matriz com os locais dos navios
+    for (int i = 0; i < linha; i++){
+        printf("%d |", i);
+    for (int j = 0; j < coluna; j++){
+        if (i == j && i < 3){
+            printf(" 3");
+    } else if(i + j == 9 && i > 6){
+            printf(" 3");
+    } else{
+            printf(" %d", matriz[i][j]);
+    }}
+            printf("\n");
     }
-    printf("\n");
-
-    // Exibir tabuleiro com numeração na vertical
-    for (int i = 0; i < 10; i++) {
-        printf("%2d | ", i); // Número da linha à esquerda
-        for (int j = 0; j < 10; j++) {
-            printf("%d  ", tabuleiro[i][j]); // Exibir valores da matriz
-        }
-        printf("\n");
-    }
-
-    return 0;
+        
+    
+    
+return 0;
 }
